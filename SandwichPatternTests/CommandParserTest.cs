4a4489453d2756e09a0f97a6dfa204ad2d5561b2
Clement <clementhottotpro@gmail.com>
2022-05-30 22:51:22 +0200

    test for CommandParser

--- SandwichPatternTests/CommandParserTest.cs
+++ SandwichPatternTests/CommandParserTest.cs
@@ -6,13 +6,14 @@ public class CommandParserTest
 {
     private CommandParser _comandParser;
     private Sandwich _sandwich1;
+    private Sandwich _sandwich2;
 
     [SetUp]
     public void Setup()
     {
         _sandwich1 = new Sandwich("sandwich1", new Dictionary<Ingredient, int>());
-
-        var sandwiches = new List<Sandwich> { _sandwich1 };
+        _sandwich2 = new Sandwich("sandwich2", new Dictionary<Ingredient, int>());
+        var sandwiches = new List<Sandwich> { _sandwich1, _sandwich2 };
 
         _comandParser = new CommandParser(sandwiches);
     }
@@ -24,10 +25,10 @@ public class CommandParserTest
         var expected = new Dictionary<Sandwich, int> { { _sandwich1, 1 } };
 
         var res = _comandParser.parse(inputs);
-        
+
         Assert.That(expected, Is.EqualTo(res));
     }
-    
+
     [Test]
     public void should_get_two_sandwich1()
     {
@@ -35,7 +36,29 @@ public class CommandParserTest
         var expected = new Dictionary<Sandwich, int> { { _sandwich1, 2 } };
 
         var res = _comandParser.parse(inputs);
-        
+
+        Assert.That(expected, Is.EqualTo(res));
+    }
+
+    [Test]
+    public void should_get_one_sandwich1_and_one_sandwich2()
+    {
+        string[] inputs = { "1", _sandwich1.Name, "1", _sandwich2.Name };
+        var expected = new Dictionary<Sandwich, int> { { _sandwich1, 1 }, { _sandwich2, 1 } };
+
+        var res = _comandParser.parse(inputs);
+
+        Assert.That(expected, Is.EqualTo(res));
+    }
+    
+    [Test]
+    public void should_get_Two_sandwich1_and_three_sandwich2()
+    {
+        string[] inputs = { "1", _sandwich2.Name, "2", _sandwich1.Name, "2", _sandwich2.Name };
+        var expected = new Dictionary<Sandwich, int> { { _sandwich1, 2 }, { _sandwich2, 3 } };
+
+        var res = _comandParser.parse(inputs);
+
         Assert.That(expected, Is.EqualTo(res));
     }
 }
