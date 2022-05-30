35e25925f7d2609060dc28ead01f884642db95a8
Clement <clementhottotpro@gmail.com>
2022-05-30 23:37:32 +0200

    CommandInvoice

--- SandwichPatterns/CommandInvoice.cs
+++ SandwichPatterns/CommandInvoice.cs
@@ -2,15 +2,33 @@ namespace SandwichPatterns;
 
 public class CommandInvoice
 {
-    private Dictionary<Ingredient, double> _ingredientPrices;
+    private Dictionary<Sandwich, double> _ingredientPrices;
 
-    public CommandInvoice(Dictionary<Ingredient, double> ingredientPrices)
+    public CommandInvoice(Dictionary<Sandwich, double> ingredientPrices)
     {
         this._ingredientPrices = ingredientPrices;
     }
 
-    public void display(Dictionary<Sandwich, int> sandwich)
+    public void display(Dictionary<Sandwich, int> command)
     {
-        throw new NotImplementedException();
+        double sum = 0;
+
+        foreach (var sandwich in command)
+        {
+            if (!_ingredientPrices.ContainsKey(sandwich.Key))
+            {
+                throw new Exception("undefined sandwich");
+            }
+
+            sum += _ingredientPrices[sandwich.Key] * sandwich.Value;
+            
+            Console.WriteLine(sandwich.Value + " " + sandwich.Key.Name);
+
+            foreach (var ingredient in sandwich.Key.Ingredients)
+            {
+                Console.WriteLine("\t"+ingredient.Key.Name);
+            }
+            
+        }
     }
 }
