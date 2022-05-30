1dfc19d67f025cb7cd03169d54a269b737699f0d
Chottot <71697340+Chottot@users.noreply.github.com>
2022-05-30 16:43:17 +0200

    init test

---
+++ SandwichPatternTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SandwichPatternTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        SandwichPatterns.Sandwich test;
+        Assert.Pass();
+    }
+}
