3857c319cf7f66554edfff9bc6f25f74019357b4
Chottot <71697340+Chottot@users.noreply.github.com>
2022-05-31 16:53:56 +0200

    added price and currency

---
+++ SandwichPatterns/Price.cs
@@ -0,0 +1,19 @@
+﻿namespace SandwichPatterns;
+
+public class Price
+{
+    private Currency Currency { get; }
+
+    public double Value { get; }
+
+    public Price(Currency currency, double value)
+    {
+        Currency = currency;
+        Value = value;
+    }
+
+    public override string ToString()
+    {
+        return Value + " " + Currency;
+    }
+}
