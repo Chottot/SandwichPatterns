b4e49f1b03c8b898b1fca9870b4f6ded588477a4
Chottot <71697340+Chottot@users.noreply.github.com>
2022-05-31 16:28:14 +0200

    added quantity

---
+++ SandwichPatterns/Quantity.cs
@@ -0,0 +1,19 @@
+﻿namespace SandwichPatterns;
+
+public class Quantity
+{
+    private QuantityUnit Unit { get; }
+
+    private double Value { get; }
+    
+    public Quantity(QuantityUnit unit, double value)
+    {
+        Unit = unit;
+        Value = value;
+    }
+    
+    public override string ToString()
+    {
+        return Value +" "+ Unit ;
+    }
+}
---
+++ SandwichPatterns/QuantityUnit.cs
@@ -0,0 +1,20 @@
+﻿namespace SandwichPatterns;
+
+public class QuantityUnit
+{
+    private string Str { get; }
+    
+    private QuantityUnit(string str)
+    {
+        Str = str;
+    }
+    
+    public static QuantityUnit Gram => new QuantityUnit("g");
+    public static QuantityUnit Slice => new QuantityUnit("slice");
+    public static QuantityUnit Unit => new QuantityUnit("");
+
+    public override string ToString()
+    {
+        return Str;
+    }
+}
